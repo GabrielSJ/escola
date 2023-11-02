36014741dc22a1ff36c3652798bb1968604e1055
Gabriel Strada <gabrielsjanuario@gmail.com>
2023-11-01 22:38:59 -0300

    adicionado swagger

--- Escola/Program.cs
+++ Escola/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<EscolaContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction()) // Ou apenas 'if (true)'
 {
     app.UseSwagger();
     app.UseSwaggerUI();
