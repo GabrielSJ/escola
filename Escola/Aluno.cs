c29b914c67bbd7019979ce471939ea4574b39fe1
Gabriel Strada <gabrielsjanuario@gmail.com>
2023-11-01 22:24:19 -0300

    first commit

---
+++ Escola/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Escola
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        public int Idade { get; set; }
+
+        public decimal NotaPrimeiroSemestre { get; set; }
+
+        public decimal NotaSegundoSemestre { get; set; }
+
+        [StringLength(100)]
+        public string NomeDoProfessor { get; set; }
+
+        public int NumeroDaSala { get; set; }
+    }
+}
---
+++ Escola/EscolaContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Escola
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext(DbContextOptions<EscolaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>(entity =>
+            {
+                entity.ToTable("alunos"); // Use o esquema correto onde sua tabela está localizada.
+
+                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Nome).IsRequired().HasColumnName("nome");
+                entity.Property(e => e.Idade).IsRequired().HasColumnName("idade");
+                entity.Property(e => e.NotaPrimeiroSemestre).IsRequired().HasColumnName("notaprimeirosemestre");
+                entity.Property(e => e.NotaSegundoSemestre).IsRequired().HasColumnName("notasegundosemestre");
+                entity.Property(e => e.NomeDoProfessor).IsRequired().HasColumnName("nomedoprofessor");
+                entity.Property(e => e.NumeroDaSala).IsRequired().HasColumnName("numerodasala");
+
+                // ... e assim por diante para as outras propriedades/colunas.
+            });
+        }
+    }
+}
